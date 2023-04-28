4494cd5f9aaf7f41907fa0765d4060580f65355b
samuel diel <samuelsdiel@gmail.com>
2023-04-28 15:13:38 -0300

    padronizando e revisando os returns das requisições

--- apiPessoas/src/controllers/Pessoa.controller.ts
+++ apiPessoas/src/controllers/Pessoa.controller.ts
@@ -8,42 +8,24 @@ const repositoryPessoa = new PessoaRepository();
 
 export class PessoaController {
     async listar(request: Request, response: Response) {
-        const result = await repositoryPessoa.listar()
-        console.log(result)
+        const result = await repositoryPessoa.listar();
 
-        return response.json({
-            status: 'ok',
-            content: result
-        })
+        return response.json({ status: 'ok', content: result })
     }
 
     async Buscar(request: Request, response: Response) {
         const result = await repositoryPessoa.buscarPorId(request.params.idPessoa)
-        console.log(result)
 
-        return response.json({
-            status: 'ok',
-            content: result
-        })
+        return response.json({ status: result.status, content: result.content })
     }
 
     async cadastrar(request: Request, response: Response) {
         let { cep, nome, idade } = request.body;
         let endereco: Endereco = (await Axios.get(`http://localhost:3000/getApi/${cep}`)).data.content;
         let pessoa: Pessoa = { nome, idade, endereco }
-        const pessoas = await repositoryPessoa.incluir(pessoa);
+        const result = await repositoryPessoa.incluir(pessoa);
 
-        if (pessoas) {
-            return response.json({
-                status: 'ok',
-                content: 'Pessoa cadastrada com sucesso!'
-            })
-        } else {
-            return response.json({
-                status: 'error',
-                content: 'Algo deu errado ao cadastrar pessoa!'
-            })
-        }
+        return response.json({ status: result.status, content: result.content })
 
     }
 
@@ -51,27 +33,15 @@ export class PessoaController {
         let { cep, nome, idade, idPessoa } = request.body;
         let endereco: Endereco = (await Axios.get(`http://localhost:3000/getApi/${cep}`)).data.content;
         let pessoa: Pessoa = { nome, idade, endereco }
-        const pessoas = await repositoryPessoa.atualizar(pessoa, idPessoa);
+        console.log(pessoa);
+        const result = await repositoryPessoa.atualizar(pessoa, idPessoa);
 
-        if(pessoas) {
-            return response.json({
-                status: 'ok',
-                content: 'Pessoa atualizada com sucesso!'
-            })
-        } else {
-            return response.json({
-                status: 'error',
-                content: 'Algo deu errado ao atualizar pessoa!'
-            })
-        }
+        return response.json({ status: result.status, content: result.content })
     }
 
     async deletar(request: Request, response: Response) {
-        const pessoas = await repositoryPessoa.deletar(request.params.idPessoa);
+        const result = await repositoryPessoa.deletar(request.params.idPessoa);
 
-        return response.json({
-            status: 'ok',
-            content: 'Pessoa deletada com sucesso!'
-        })
+        return response.json({ status: result.status, content: result.content })
     }
 }
--- apiPessoas/src/data/Pessoa.repository.ts
+++ apiPessoas/src/data/Pessoa.repository.ts
@@ -7,44 +7,63 @@ export class PessoaRepository {
     async listar(): Promise<Pessoa[]> {
         let result = await PessoaModelTable.findAll()
         pessoas = []
-        for(let d in result) {
+        for (let d in result) {
             pessoas.push(result[d].dataValues)
         }
         return pessoas
     }
 
-    async incluir(pessoa: Pessoa): Promise<Pessoa> {
+    async incluir(pessoa: Pessoa) {
         const insert = await PessoaModelTable.create({
             nome: pessoa.nome,
             idade: pessoa.idade,
             cep: pessoa.endereco.cep,
             logradouro: pessoa.endereco.logradouro,
             complemento: pessoa.endereco.complemento,
-            bairro: pessoa.endereco.bairro,
+            //bairro: pessoa.endereco.bairro,
             localidade: pessoa.endereco.localidade,
             uf: pessoa.endereco.uf,
             ibge: pessoa.endereco.ibge,
             gia: pessoa.endereco.gia,
             ddd: pessoa.endereco.ddd,
             siafi: pessoa.endereco.siafi,
-        })
-        return pessoa
+        });
+
+        if (insert) {
+            return {
+                status: 'ok',
+                content: 'Pessoa cadastrada com sucesso!'
+            }
+        } else {
+            return {
+                status: 'error',
+                content: 'Erro ao castrasdar essa pessoa!'
+            }
+        }
     }
 
-    async buscarPorId(idPessoa: number): Promise<Pessoa[]> {
+    async buscarPorId(idPessoa: number) {
         let result = await PessoaModelTable.findOne({
             where: {
                 idPessoa: idPessoa
             }
         });
-        if(result) {
-            return result.dataValues;
+        if (result) {
+            return {
+                status: 'ok',
+                content: result.dataValues
+            }
+        } else {
+            return {
+                status: 'error',
+                content: 'Pessoa não encontrada'
+            }
         }
     }
 
-    async atualizar(pessoa: Pessoa, idPessoa: number): Promise<Pessoa> {
-        
-        await PessoaModelTable.update({
+    async atualizar(pessoa: Pessoa, idPessoa: number) {
+
+       let update = await PessoaModelTable.update({
             nome: pessoa.nome,
             idade: pessoa.idade,
             cep: pessoa.endereco.cep,
@@ -63,16 +82,39 @@ export class PessoaRepository {
             }
         })
 
-        return pessoa
+        if (update) {
+            return {
+                status: 'ok',
+                content: 'Pessoa atualizada com sucesso!'
+            }
+        } else {
+            return {
+                status: 'error',
+                content: 'Erro ao atualizar essa pessoa!'
+            }
+        }
     }
 
     async deletar(idPessoa: string) {
-        
-        await PessoaModelTable.destroy({
+
+        let result = await PessoaModelTable.destroy({
             where: {
                 idPessoa: idPessoa
             }
         });
+        if (result > 0) {
+            return {
+                status: 'ok',
+                content: 'Pessoa deletada com sucesso!'
+            }
+        } else {
+            return {
+                status: 'error',
+                content: 'Algo deu errado ao deletar essa pessoa!'
+            }
+        }
+
+
     }
 
 }
