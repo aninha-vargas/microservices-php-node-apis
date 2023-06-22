e695a2aa0b39b1fb6ce76dac226ac961cd4d3647
Ana Maria Vargas <aninha_vargas@hotmail.com>
2023-06-21 22:00:59 -0300

    ajustes node

--- apiPessoas/src/services/Rabbitmq.services.ts
+++ apiPessoas/src/services/Rabbitmq.services.ts
@@ -3,7 +3,8 @@ import * as amqp from 'amqplib';
 export class RabbitMQService {
   async enviar(mensagem: string): Promise<any> {
     const fila = "FILA";
-    const rabbitURL = "amqps://cslpkmzd:AvZdJN4xJsnhPDBivWrUVXt7VUcL8B7G@jackal.rmq.cloudamqp.com/cslpkmzd";
+    const rabbitURL = "amqp://localhost";
+    // const rabbitURL = "amqps://cslpkmzd:AvZdJN4xJsnhPDBivWrUVXt7VUcL8B7G@jackal.rmq.cloudamqp.com/cslpkmzd";
 
     try {
       const connection = await amqp.connect(rabbitURL);
